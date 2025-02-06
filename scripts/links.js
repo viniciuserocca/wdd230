0c75f2fee03157739ae6439c7015f0fcd6270c81
Vinicius Rocca <vinicius.erocca@gmail.com>
2025-02-06 03:59:16 -0300

    team activity completed

--- scripts/links.js
+++ scripts/links.js
@@ -1,12 +1,47 @@
 const baseURL = "https://viniciuserocca.github.io/wdd230/";
-const linksURL = "https://viniciusrocca.github.io/wdd230/data/links.json";
-
+const linksURL = "https://viniciuserocca.github.io/wdd230/data/links.json";
+const linkList = document.querySelector('#link-list');
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
+    try {
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            displayLinks(data.weeks);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    } 
+    catch (error) {
+        console.error(error);
+    }
 }
+
+const displayLinks = (weeks) => {
+    weeks.forEach(week => {
+        let line = document.createElement('li');
+        line.textContent = `${week.week}: `;
+
+        week.links.forEach((link, index) => {
+            const linkElement = document.createElement('a');
+            if (link.url.includes("https://")) {
+                linkElement.href = link.url;
+            } else {
+                linkElement.href = `${baseURL}${link.url}`;
+            }
+            linkElement.textContent = link.title;
+
+            line.appendChild(linkElement);
+
+            if (index < week.links.length - 1) {
+                line.append(" | ");
+            }
+        });
+
+        linkList.appendChild(line);
+    });
+} 
   
 getLinks();
   
