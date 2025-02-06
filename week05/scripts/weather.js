cf6492dad271665d1197cea8c85c9d804546be16
Vinicius Rocca <vinicius.erocca@gmail.com>
2025-02-06 01:49:00 -0300

    weather unit changed to metric

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.7494660166711&lon=6.635724293984299&units=imperial&appid=bf4bfee1ad11f82006a74a4d5990e597";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.7494660166711&lon=6.635724293984299&units=metric&appid=bf4bfee1ad11f82006a74a4d5990e597";
 
 async function apiFetch() {
     try {
