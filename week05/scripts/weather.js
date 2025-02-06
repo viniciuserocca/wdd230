eb1af61239a2c143605b74b0e4602ddca72f4783
Vinicius Rocca <vinicius.erocca@gmail.com>
2025-02-06 01:45:45 -0300

    week05 learning activies added

---
+++ week05/scripts/weather.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.7494660166711&lon=6.635724293984299&units=imperial&appid=bf4bfee1ad11f82006a74a4d5990e597";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    } 
+    catch (error) {
+        console.error(error);
+    }
+}
+
+function displayResults (data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt',`${desc} icon`);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
